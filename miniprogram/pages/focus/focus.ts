2ddf0226d611e7c09eab0c29c5d2c3de984b75c7
陌离 <h2892197119@foxmail.com>
2025-08-09 11:32:59 +0800

    250809-专注页面：滑动开关实现

--- miniprogram/pages/focus/focus.ts
+++ miniprogram/pages/focus/focus.ts
@@ -1,66 +1,79 @@
 // pages/focus/focus.ts
 Page({
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        slider_x: 0,
+    },
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+    onSliderEnd() {
+        if (this.data.slider_x > 220) {
+            console.log("拖动成功，在此处添加开启专注的逻辑")
+        }
 
-  },
+        this.setData({
+            slider_x : 0
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad() {
+    onSliderChange(event: WechatMiniprogram.MovableViewChange) {
+        this.data.slider_x = event.detail.x;
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad() {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady() {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow() {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide() {
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
+    },
 
-  },
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload() {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
+    },
 
-  },
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh() {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
+    },
 
-  },
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom() {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
+    },
 
-  }
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage() {
+
+    }
 })
